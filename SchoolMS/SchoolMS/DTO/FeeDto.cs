323748df4d2d40c3c1c7abfdff8d979e78be2360
Hamed Ahmed <ha9597ma@gmail.com>
2024-10-15 22:18:24 +0300

    Fee Edit

--- SchoolMS/SchoolMS/DTO/FeeDto.cs
+++ SchoolMS/SchoolMS/DTO/FeeDto.cs
@@ -7,6 +7,6 @@
         public int NumberOfInstallments { get; set; }
         public int StudentId { get; set; }
         public decimal AmountPerInstallment { get; set; } // Add this if it's used in calculations
-
+        public decimal RemainingBalance { get; internal set; }
     }
 }
